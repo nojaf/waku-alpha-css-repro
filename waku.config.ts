60c5517adb6ef3dbba8be3e3193bca7aeefc5321
nojaf <florian.verdonck@outlook.com>
2025-08-04 09:46:50 +0200

    Try waku.config.ts

---
+++ waku.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "waku/config"
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
+  }
+})
